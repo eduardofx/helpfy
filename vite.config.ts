3915417c36abe730e1af1b5f3577341272e7054c
gustavotassi <souzagustavotassi@gmail.com>
2026-05-31 22:39:03 -0300

    fix: remove vite single file plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/",
-  plugins: [
-    react(),
-    viteSingleFile(),
-  ],
+  plugins: [react()],
   server: {
     host: "::",
     port: 8080,
   },
   build: {
     outDir: "dist",
-    assetsDir: "",
-    cssCodeSplit: false,
   },
   resolve: {
     alias: {
